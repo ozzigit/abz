89225cda0b5942b7c0dd3a7b557da26b1c02730b
ozzigit <ozzi@ukr.net>
2022-09-07 19:35:16 +0300

    models.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     DEBUG = False
     CSRF_ENABLED = True
-    # WTF_CSRF_SECRET_KEY = '1234567890'
-    # SECRET_KEY = '0987654321'
+    WTF_CSRF_SECRET_KEY = '1234567890'
+    SECRET_KEY = '0987654321'
     SQLALCHEMY_DATABASE_URI = "postgresql://abz_user:abz_pass@localhost:5432/abz_db"
 
 
--- pip_install.sh
+++ pip_install.sh
@@ -3,3 +3,4 @@ python3 -m venv venv
 source venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
+export FLASK_APP=microblog.py
