ae7aa46cb377b7fb3b426362a79570ec54b251ed
ozzigit <ozzi@ukr.net>
2022-09-07 12:12:24 +0300

    models.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,7 @@ from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
-# db = SQLAlchemy(app)
-# migrate = Migrate(app, db)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-
-from app import routes#, models
+from app import routes, models
--- app/models.py
+++ app/models.py
@@ -1,10 +1,8 @@
-from flask_sqlalchemy import SQLAlchemy
+from app import db
 from datetime import datetime
 
-db = SQLAlchemy()
 
-
-class CarsModel(db.Model):
+class Employee(db.Model):
     __tablename__ = 'employee'
 
     id = db.Column(db.Integer, nullable=False, unique=True, primary_key=True, autoincrement=True)
@@ -21,3 +19,19 @@ class CarsModel(db.Model):
         self.date_join = date_join
         self.wage = wage
         self.chief = chief
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False, unique=True, autoincrement=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def __init__(self, id, username, email, password_hash):
+        self.id = id
+        self.username = username
+        self.email = email
+        self.password_hash = password_hash
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
