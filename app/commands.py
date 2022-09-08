d356754d5c8cdf2afd095ad29889af28c91eb581
ozzigit <ozzi@ukr.net>
2022-09-08 09:31:23 +0300

    commands skeleton

--- app/commands.py
+++ app/commands.py
@@ -1,13 +1,52 @@
-from app import app
+from app import app, db
+import random
+from faker import Faker
+from models import Employee
+
+
+class Commands:
+
+    @classmethod
+    def add_employees_in_bd(cls):
+        """ Adds workers with random names to the database
+        """
+        workers_previous_hierarchy = []
+        list_workers = [1, 2, 3, 4, 5]
+
+        for hierarchy in range(5):
+            _workers_created = []
+            for i in range(list_workers[hierarchy]):
+                new_employee = Employee.objects.create(**cls.__random_dict_employee(workers_previous_hierarchy))
+                _workers_created.append(new_employee)
+
+            workers_previous_hierarchy = _workers_created
+        db.session.commit()
+
+    @staticmethod
+    def __random_dict_employee(chief_model_list=None):
+        """ Returns a dictionary with random values to fill in the Employee model. """
+        faker = Faker('uk_UA')
+        random_object = random.choice(chief_model_list) if chief_model_list else chief_model_list
+        return {
+            'name': faker.name(),
+            'work_position': faker.job(),
+            'wage': random.randint(3000, 4000),
+            'chief': random_object
+        }
+
+    @classmethod
+    def clear_db(cls, model):
+        db.session.query(model).delete()
+        db.session.commit()
 
 
 @app.cli.command("init_db")
 def init_db():
     """set values in db"""
-    print('fdfsdfsd')
+    Commands.add_employees_in_bd()
 
 
 @app.cli.command("clear_db")
 def del_tables():
     """del all tables in db"""
-    print("all tables is deleted")
+    Commands.clear_db(Employee)
