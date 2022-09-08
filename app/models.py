49d9a6e6c8faa66a027d341d0ec69c3c52e12a85
ozzigit <ozzi@ukr.net>
2022-09-08 20:11:23 +0300

    login and registration

--- app/models.py
+++ app/models.py
@@ -1,6 +1,13 @@
-from app import db
+from app import db, login
 from sqlalchemy import Identity
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+
+@login.user_loader
+def load_user(id):
+    return Users.query.get(int(id))
 
 
 class Employee(db.Model):
@@ -13,28 +20,21 @@ class Employee(db.Model):
     wage = db.Column(db.Float(precision=10, decimal_return_scale=2), default=0.01)
     chief = db.Column(db.ForeignKey("employee.id"), default=None)
 
-    # def __init__(self, id, name, work_position, date_join, wage, chief):
-    #     self.id = id
-    #     self.name = name
-    #     self.work_position = work_position
-    #     self.date_join = date_join
-    #     self.wage = wage
-    #     self.chief = chief
     def __repr__(self):
         return '<Name {}, id {}, chief {}>'.format(self.name, self.id, self.chief)
 
 
-class Users(db.Model):
+class Users(UserMixin, db.Model):
     id = db.Column(db.Integer, Identity(start=1), primary_key=True, nullable=False, unique=True, autoincrement=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
 
-    # def __init__(self, id, username, email, password_hash):
-    #     self.id = id
-    #     self.username = username
-    #     self.email = email
-    #     self.password_hash = password_hash
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
