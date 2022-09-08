8b6fca2fb17a769265eb33658ad09e4f2f4d7dc7
ozzigit <ozzi@ukr.net>
2022-09-08 10:49:16 +0300

    commands skeleton

--- app/commands.py
+++ app/commands.py
@@ -30,7 +30,7 @@ class Commands:
     def __random_dict_employee(chief_model_list=None):
         """ Returns a dictionary with random values to fill in the Employee model. """
         faker = Faker('uk_UA')
-        random_object = random.choice(chief_model_list) if chief_model_list is not None else chief_model_list
+        random_object = random.choice(chief_model_list) if chief_model_list is not None else [None]
         return {
             'name': faker.name(),
             'work_position': faker.job(),
